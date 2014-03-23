c87fa460661f4ad847762612539447b50c0f5b0e
Adam Singer <financeCoding@gmail.com>
2014-03-22 19:03:04 -0700

    Automated update

--- lib/gan_v1beta1_api_browser.dart
+++ lib/gan_v1beta1_api_browser.dart
@@ -8,12 +8,6 @@ import "package:google_gan_v1beta1_api/gan_v1beta1_api_client.dart";
 /** Lets you have programmatic access to your Google Affiliate Network data. */
 class Gan extends Client with BrowserClient {
 
-  /** OAuth Scope2: Manage your GAN data */
-  static const String GAN_SCOPE = "https://www.googleapis.com/auth/gan";
-
-  /** OAuth Scope2: View your GAN data */
-  static const String GAN_READONLY_SCOPE = "https://www.googleapis.com/auth/gan.readonly";
-
   final oauth.OAuth2 auth;
 
   Gan([oauth.OAuth2 this.auth]);
--- lib/gan_v1beta1_api_console.dart
+++ lib/gan_v1beta1_api_console.dart
@@ -9,12 +9,6 @@ import "package:google_gan_v1beta1_api/gan_v1beta1_api_client.dart";
 /** Lets you have programmatic access to your Google Affiliate Network data. */
 class Gan extends Client with ConsoleClient {
 
-  /** OAuth Scope2: Manage your GAN data */
-  static const String GAN_SCOPE = "https://www.googleapis.com/auth/gan";
-
-  /** OAuth Scope2: View your GAN data */
-  static const String GAN_READONLY_SCOPE = "https://www.googleapis.com/auth/gan.readonly";
-
   final oauth2.OAuth2Console auth;
 
   Gan([oauth2.OAuth2Console this.auth]);
